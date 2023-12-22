894846dba549903e2a70bc141b49825072f56807
SuperJK55 <stepan.kolenkin@mail.ru>
2023-12-23 02:28:26 +0600

    Change text in check out our latest article block

--- src/components/our-latest-article/OurLatestArticle.tsx
+++ src/components/our-latest-article/OurLatestArticle.tsx
@@ -14,14 +14,14 @@ interface IOurLatestArticle {
 const OurLatestArticle: React.FC<IOurLatestArticle> = ({ arr }) => {
   return (
     <div className={classes.article_block}>
-      <h1 className={classes.title_text}>Our services</h1>
+      <h1 className={classes.title_text}>Check out our latest article</h1>
       <div className={classes.divider}></div>
       <div className={classes.row}>
         {arr.map((item, index) => (
           <div key={index} className={classes.cardMaterial}>
             <img className={classes.promo} src={arr[index].img} />
-            <h1 className={classes.h1}>{arr[index].title}</h1>
-            <h2 className={classes.h2}>{arr[index].subtitle}</h2>
+            <h1 className={classes.title_article}>{arr[index].title}</h1>
+            <h2 className={classes.subtitle_article}>{arr[index].subtitle}</h2>
             <button className={classes.moreButton}>Read more ➜</button>
           </div>
         ))}
